c06f00b3aaab52d115445b57aa596759cfb1afc7
Syrine <syrine.bouchelleghem@gmail.com>
2022-03-01 20:32:49 +0100

    insert all the products into this database

--- server/server.js
+++ server/server.js
@@ -1,21 +1,55 @@
 const {MongoClient} = require('mongodb');
 const MONGODB_URI = 'mongodb+srv://clearfashion:clearfashion3210@clearfashion.6ubr1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const MONGODB_DB_NAME = 'clearfashion';
-const client = new MongoClient(MONGODB_URI);
-
+const client = new MongoClient(MONGODB_URI, {'useNewUrlParser': true});
+const products = require('./all_products.json')
 
 
 
 async function main() {
-  
+
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db(MONGODB_DB_NAME);
-
   return 'done.';
 }
 
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+async function clearCollection(){
+  try{
+    await client.connect();
+    const db = client.db(MONGODB_DB_NAME);
+    const collection = db.collection('products')
+    const result = await collection.deleteMany({});
+    console.log(`collection products from ${MONGODB_DB_NAME} is cleared`)
+    console.log(result)
+  }
+  catch(error){console.log(error)}
+  finally{
+    await client.close()
+  }
+}
+
+
+async function insertData(){
+  try{
+    await client.connect();
+    const db = client.db(MONGODB_DB_NAME);
+    const collection = db.collection('products')
+    const result = await collection.insertMany(products)
+    console.log(`data were added to collection products of database ${MONGODB_DB_NAME}`)
+    console.log(result)
+  }
+  catch(error){console.log(error)}
+  finally{
+    await client.close()
+  }
+}
+
+//clearCollection()
+insertData();
+
+// main()
+//   .then(console.log)
+//   .catch(console.error)
+//   .then(insertData())
+//   .finally(() => client.close());
