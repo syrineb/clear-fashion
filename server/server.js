74c9fdffac954db4b3d9dce5528eac1a75740674
Syrine <syrine.bouchelleghem@gmail.com>
2022-03-06 17:59:35 +0100

    feat(new-products):Find all products related to a given brands

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ const MONGODB_URI = 'mongodb+srv://clearfashion:clearfashion3210@clearfashion.6u
 const MONGODB_DB_NAME = 'clearfashion';
 const client = new MongoClient(MONGODB_URI, {'useNewUrlParser': true});
 const products = require('./all_products.json')
-
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 
 async function main() {
@@ -44,9 +47,21 @@ async function insertData(){
     await client.close()
   }
 }
-
-//clearCollection()
-insertData();
+//FIND ALL PRODUCT RELATED TO A GIVEN brand
+async function sortByBrand(){
+  await client.connect();
+  const db = client.db(MONGODB_DB_NAME);
+  const collection = db.collection('products')
+  readline.question('Which brand?   ', brand => {
+  console.log(brand);
+  readline.close();});
+  const products = await collection.find({brand}).toArray();
+  console.log(products);
+  await client.close()
+}
+sortByBrand();
+// clearCollection()
+// insertData();
 
 // main()
 //   .then(console.log)
